6c714cd562df5259bff6807ed435fc8e6bad15f2
Katelyn Mehner <kmehner@gmail.com>
2022-06-29 11:47:20 -0600

    Customized button with objects passed through the array

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import React from 'react'
-import { StyleSheet, SafeAreaView, Button } from 'react-native';
+import { StyleSheet, SafeAreaView, Button, Alert } from 'react-native';
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <Button 
-        color="orange"
         title="Click Me" 
-        onPress={() => alert("Button tapped")} />
+        onPress={() => Alert.alert("My title", "My message", [
+          {text: "Yes", onPress: () => console.log("Yes") },
+          { text: "No", onPress: () => console.log("No")},
+        ])} />
     </SafeAreaView>
   );
 }
