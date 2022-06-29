5435d5ff6e73b088e4b695c576e4bff290bb1d1e
Katelyn Mehner <kmehner@gmail.com>
2022-06-29 13:25:17 -0600

    Children view connected to parent view

--- App.js
+++ App.js
@@ -1,30 +1,26 @@
 import React from 'react'
-import { StyleSheet, 
-  SafeAreaView, 
-  View, 
-  Platform, 
-  StatusBar, 
-  } from 'react-native';
-import { useDimensions, useDeviceOrientation } from '@react-native-community/hooks';
+import { View } from 'react-native';
 
 export default function App() {
-  const { landscape } = useDeviceOrientation();
-
   return (
-    <SafeAreaView style={styles.container}>
+    <View 
+      style={{
+        backgroundColor: "#fff",
+        flex: 1, 
+      }}
+    >
       <View style={{
-        backgroundColor: 'dodgerblue',
-        width: '100%',
-        height: landscape ? "100%" : "30%",
-      }}></View>
-    </SafeAreaView>
+        backgroundColor: "dodgerblue",
+        flex: 1,
+      }} />
+      <View style={{
+        backgroundColor: "gold",
+        flex: 1,
+      }} />
+      <View style={{
+        backgroundColor: "tomato",
+        flex: 1,
+      }} />
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0
-  },
-});
