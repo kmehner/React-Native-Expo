c61aae74781984a170439a7ee9a396bbd559fc36
Katelyn Mehner <kmehner@gmail.com>
2022-06-30 16:46:29 -0600

    reconfigured into App folder

--- App.js
+++ App.js
@@ -1,37 +1,7 @@
-import React from 'react'
-import { View } from 'react-native';
+import React from "react";
+import { View } from "react-native";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
-  return (
-    <View 
-      style={{
-        backgroundColor: "#fff",
-        flex: 1,
-        flexDirection: "row", // horizontal 
-        justifyContent: "center", // main 
-        alignContent: "center", // very center
-        flexWrap: "wrap", // when enabled, alignItems determins the alignment within each line 
-      }}
-    >
-      <View style={{
-        backgroundColor: "dodgerblue",
-        width: 100,
-        height: 100,
-      }} />
-      <View style={{
-        backgroundColor: "gold",
-        width: 100,
-        height: 100,
-        top: 20, // Moves 20 indepenent pixels from the top
-        // ^ Does not affect the layout around it (top, bottom, left, right)
-        left: 20,
-        position: "relative",
-      }} />
-      <View style={{
-        backgroundColor: "tomato",
-        width: 100,
-        height: 100,
-      }} />
-    </View>
-  );
+  return <WelcomeScreen />;
 }
