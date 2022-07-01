4986f14783b645ee0368a7eba1a07d132e499642
Katelyn Mehner <kmehner@gmail.com>
2022-07-01 12:30:02 -0600

    added config - restarting welcome page, added loginbutton

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { ImageBackground, StyleSheet, View, Image, Text } from "react-native";
+import React from 'react';
+import { View, ImageBackground, StyleSheet } from 'react-native';
 
 function WelcomeScreen(props) {
   return (
@@ -7,41 +7,21 @@ function WelcomeScreen(props) {
       style={styles.background}
       source={require("../assets/background.jpg")}
     >
-      <View style={styles.logoContainer}>
-        <Image style={styles.logo} source={require("../assets/logo-red.png")} />
-        <Text>Sell What You Don't Need</Text>
-      </View>
-      <View style={styles.loginButton}></View>
-      <View style={styles.registerButton}></View>
+      <View style={styles.loginButton} ></View>
     </ImageBackground>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   background: {
     flex: 1,
-    justifyContent: "flex-end",
-    alignItems: "center",
+    justifyContent: 'flex-end',
   },
   loginButton: {
-    width: "100%",
+    width: '100%',
     height: 70,
     backgroundColor: "#fc5c65",
-  },
-  logo: {
-    width: 100,
-    height: 100,
-  },
-  logoContainer: {
-    position: "absolute",
-    top: 70,
-    alignItems: "center",
-  },
-  registerButton: {
-    width: "100%",
-    height: 70,
-    backgroundColor: "#4ecdc4",
-  },
-});
+  }
+})
 
 export default WelcomeScreen;
