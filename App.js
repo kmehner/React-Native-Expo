11f521d2d290c4860af6495107e0132105b91fd8
Katelyn Mehner <kmehner@gmail.com>
2022-07-01 17:26:11 -0600

    Shadows and radius

--- App.js
+++ App.js
@@ -1,8 +1,29 @@
 import React from "react";
 import { View } from "react-native";
-import ViewImageScreen from "./app/screens/ViewImageScreen";
-import WelcomeScreen from "./app/screens/WelcomeScreen";
+
 
 export default function App() {
-  return <ViewImageScreen />;
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <View
+        style={{
+          backgroundColor: "dodgerblue",
+          width: 100,
+          height: 100,
+          shadowColor: "grey",
+          shadowOffset: { width: 10, height: 10},
+          shadowOpacity: 1,
+          shadowRadius: 10,
+          elevation: 20,
+          borderRadius: 50,
+        }}
+      ></View>
+    </View>
+  );
 }
