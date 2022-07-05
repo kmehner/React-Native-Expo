40240c14565375b6a7ceafc4f5780396f88645d0
Katelyn Mehner <kmehner@gmail.com>
2022-07-05 17:36:44 -0600

    Approach 2 - platform-specific without importing Platform

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View } from "react-native";
 import AppText from "./app/components/AppText";
 
 export default function App() {
