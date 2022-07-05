39e59ccd2ad9e4c687c224158b51b74c5b462703
Katelyn Mehner <kmehner@gmail.com>
2022-07-05 17:30:47 -0600

    Approach 1 to separating platform specific styles

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { View, Text } from "react-native";
-import {MaterialCommunityIcons} from '@expo/vector-icons'
+import AppText from "./app/components/AppText";
 
 export default function App() {
   return (
@@ -11,12 +11,7 @@ export default function App() {
         alignItems: "center",
       }}
     >
-      <MaterialCommunityIcons
-        name="email"
-        size={60}
-        color="dodgerblue"
-      />
-
+      <AppText>I love React Native!</AppText>
     </View>
   );
 }
