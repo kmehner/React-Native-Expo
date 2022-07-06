0e4e796692243a7c4c9753aebb02dc40fe3d15ec
Katelyn Mehner <kmehner@gmail.com>
2022-07-06 09:53:56 -0600

    Finished Welcome Screen styling

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -5,18 +5,20 @@ import AppButton from '../components/AppButton';
 function WelcomeScreen(props) {
   return (
     <ImageBackground
+      blurRadius={10}
       style={styles.background}
       source={require("../assets/background.jpg")}
     >
       <View style={styles.logoContainer} >
         <Image style={styles.logo} source={require('../assets/logo-red.png')}  /> 
-        <Text>Sell what you don't need</Text>
+        <Text style={styles.tagline}  >Sell what you don't need</Text>
       </View>
       <View style={styles.buttonsContainer}>
         <AppButton title="Login" />
+        <AppButton title="Register"  color="secondary"   />
       </View>
 
-      <View style={styles.registerButton} ></View>
+
     </ImageBackground>
   )
 }
@@ -28,7 +30,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   buttonsContainer: {
-    padding: 20,
+    padding: 30,
     width: '100%',
   },
   logo: {
@@ -40,11 +42,11 @@ const styles = StyleSheet.create({
     top: 70,
     alignItems: 'center',
   },
-  registerButton: {
-    width: '100%',
-    height: 70,
-    backgroundColor: "#4ecdc4",
-  }
+  tagline: {
+    fontSize: 25,
+    fontWeight: '600',
+    paddingVertical: 20,
+  },
 })
 
 export default WelcomeScreen;
