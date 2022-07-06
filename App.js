13246325eace3ccd5fa92ef898fa0a5f02a0bc68
Katelyn Mehner <kmehner@gmail.com>
2022-07-06 10:55:27 -0600

    Complete Card component

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
 import React from "react";
 import { View } from "react-native";
 
-import WelcomeScreen from "./app/screens/WelcomeScreen";
+import Card from './app/components/Card'
 
 export default function App() {
   return (
-    <WelcomeScreen />
+      <View style={{
+            backgroundColor: '#f8f4f4',
+            padding: 20,
+            paddingTop: 100,
+          }}>
+            <Card 
+              title="Red jacket for sale"
+              subTitle="100"
+              image={require("./app/assets/jacket.jpg")}
+            />
+      </View>
   );
 }
+
+
