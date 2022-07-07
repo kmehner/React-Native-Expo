f526059ee7adf541acb222948415543609ce4936
Katelyn Mehner <kmehner@gmail.com>
2022-07-06 22:48:44 -0600

    Selection handling

--- app/screens/MessagesScreen.js
+++ app/screens/MessagesScreen.js
@@ -36,6 +36,7 @@ function MessagesScreen(props) {
                         title={item.title}
                         subTitle={item.description}
                         image={item.image}
+                        onPress={() => console.log("Message selected", item)}
                     />
                 )} 
                 ItemSeparatorComponent={ ListItemSeparator }
